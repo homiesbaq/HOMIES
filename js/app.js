08cef2535c2ff71cfdf797d8d23e56c2a74b7799
homiesbaq <gabotraslee@gmail.com>
2026-02-01 17:28:21 -0500

    app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.body.classList.add("loaded");
 });
+
+const track = document.querySelector(".carousel-track");
+const slides = document.querySelectorAll(".slide");
+
+let index = 0;
+
+setInterval(() => {
+  index++;
+  if (index >= slides.length) {
+    index = 0;
+  }
+  track.style.transform = `translateX(-${index * 100}%)`;
+}, 4000);

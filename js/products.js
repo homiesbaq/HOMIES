6d3bbb59f6b6b99b8190fdf6886a31041a4719b1
homiesbaq <gabotraslee@gmail.com>
2026-02-01 14:58:43 -0500

    Create products.js

---
+++ js/products.js
@@ -0,0 +1,19 @@
+const products = [
+  {
+    id: 1,
+    name: "Camiseta Rey Momo",
+    price: 90000,
+    image: "img/producto1.jpg",
+    colors: ["Rojo", "Negro"],
+    sizes: ["S", "M", "L"]
+  },
+  {
+    id: 2,
+    name: "Hoodie Carnaval",
+    price: 160000,
+    image: "img/producto2.jpg",
+    colors: ["Negro"],
+    sizes: ["M", "L", "XL"]
+  },
+  // copia y cambia hasta tener 10
+];
